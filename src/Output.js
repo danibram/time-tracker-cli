42690088303bb9fea46f7db1f410a7b1b471d417
Daniel Biedma <info@dbr.io>
2016-02-06 15:34:55 +0100

    fixes on the output

--- src/Output.js
+++ src/Output.js
@@ -4,20 +4,10 @@ export const humanParseDiff = function(secs) {
     let hours   = Math.floor(secs / 3600)
     let minutes = Math.floor((secs - (hours * 3600)) / 60)
     let seconds = secs - (hours * 3600) - (minutes * 60)
-    hours = (hours == 0 ? '' : (hours < 10 ? '0' + hours : hours) + 'h ')
-    minutes = (minutes == 0 ? '' : (minutes < 10 ? '0' + minutes : minutes) + 'm ')
-    seconds = (seconds  < 10 ? '0' + seconds : seconds) + 's'
+    hours = (hours == 0 ? repeatChar(' ', 4) : (hours < 10 ? '0' + hours : hours) + 'h ')
+    minutes = ((minutes == 0 && hours == '') ? repeatChar(' ', 4) : (minutes < 10 ? '0' + minutes : minutes) + 'm ')
+    seconds = (seconds == 0 ? repeatChar(' ', 4) : (seconds  < 10 ? '0' + seconds : seconds) + 's')
     return hours + minutes + seconds
-}
-
-export const outputHumanParse = function(secs) {
-    let outputDuration = humanParseDiff(secs)
-    if (outputDuration.length === 3) {
-        outputDuration = repeatChar(' ', 8) + outputDuration
-    } else if (outputDuration.length === 7) {
-        outputDuration = repeatChar(' ', 4) + outputDuration
-    }
-    return outputDuration
 }
 
 export const repeatChar = function (char, times) {
@@ -37,15 +27,15 @@ export const sumarize = function(search, tasks) {
         let duration = moment(task.task.stop).diff(moment(task.task.start), 'seconds')
         total += duration
 
-        let outputDuration = outputHumanParse(duration)
+        let outputDuration = humanParseDiff(duration)
 
         let name = task.name
-        let startTime = moment(task.task.start).format('HH:mm:ss DD/MM/YYYY')
+        let startTime = moment(task.task.start).format('DD/MM/YYYY')
 
         let time = `${outputDuration} (${startTime}) ${name}`
         output += '  ├──> ' + time + '\n'
     })
     output += `  │ \n`
-    output += `  └──> ${outputHumanParse(total)} (Total time)\n`
+    output += `  └──> ${humanParseDiff(total)} (Total time)\n`
     process.stdout.write(output)
 }
